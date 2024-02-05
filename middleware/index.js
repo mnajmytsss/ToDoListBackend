bb14eb343a29f0a116ee0048370907717c540ba8
mnajmytsss <najmytsaqiby22@gmail.com>
2024-02-05 18:31:38 +0700

    feat: implement cookie-parser

--- middleware/index.js
+++ middleware/index.js
@@ -1,17 +1,19 @@
 const corsMiddleware = require("./corsMiddleware");
-const databaseMiddleware = require('./databaseMiddleware')
+const databaseMiddleware = require("./databaseMiddleware");
 const helmetMiddleware = require("./helmetMiddleware");
 const morganMiddleware = require("./morganMiddleware");
 const requestMiddleware = require("./requestMiddleware");
-const bodyParserMiddleware = require("./bodyParserMiddleware")
+const bodyParserMiddleware = require("./bodyParserMiddleware");
+const cookieMiddleware = require("./cookieMiddleware");
 
 const useMiddleware = (app) => {
-    app.use(requestMiddleware);
-    helmetMiddleware(app);
-    bodyParserMiddleware(app);
-    corsMiddleware(app);
-    morganMiddleware(app);
-    app.use(databaseMiddleware);
-}
+  app.use(requestMiddleware);
+  helmetMiddleware(app);
+  cookieMiddleware(app);
+  bodyParserMiddleware(app);
+  corsMiddleware(app);
+  morganMiddleware(app);
+  app.use(databaseMiddleware);
+};
 
 module.exports = useMiddleware;
