27d02d9414a073acc2beb157975454cec9074141
mnajmytsss <najmytsaqiby22@gmail.com>
2024-02-10 19:34:36 +0700

    feat: dashboard page

--- routes/dashboardRoutes.js
+++ routes/dashboardRoutes.js
@@ -3,7 +3,7 @@ const { Router } = require("express");
 const router = Router();
 
 router.get("/", (req, res) => {
-    res.send("Hallo ganteng")
+    res.send("Hallo gandalf the gay")
 });
 
 module.exports = router;
